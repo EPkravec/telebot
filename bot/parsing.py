1e30b3dde3c9813edbfa269e61fd5b93897796b6
EPkravec <kravec.egor@gmail.com>
2021-09-14 21:37:33 +0400

    есть бот  + парсеры ютюба + делаю бот для отправки сообщений в вк и инста

--- bot/parsing.py
+++ bot/parsing.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 
 options = webdriver.ChromeOptions()
 options.headless = True  # тут выключение  браузера
-browser = webdriver.Chrome(keep_alive=False, options=options, executable_path=r'executable_files\chromedriver.exe')
+browser = webdriver.Chrome(keep_alive=False, options=options, executable_path=r'executables\chromedriver.exe')
 
 
 def data_link_client(browser, i, url):
